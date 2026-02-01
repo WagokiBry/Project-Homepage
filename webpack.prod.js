44fab030888cd2b5b52f60cc61ab48f45d76d594
Bryan <bryanwagoki2016@gmail.com>
2026-02-01 13:56:37 +0300

    fix: compress the images using plugins and minimizers

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,8 +1,48 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
-
   devtool: "source-map",
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+    }),
+
+    new ImageMinimizerPlugin({
+      minimizer: {
+        implementation: ImageMinimizerPlugin.imageminMinify,
+        options: {
+          plugins: [
+            ["gifsicle", { interlaced: true }],
+            ["mozjpeg", { quality: 75 }],
+            ["pngquant", { quality: [0.6, 0.8] }],
+            ["svgo", {}],
+          ],
+        },
+      },
+
+      generator: [
+        {
+          type: "asset",
+          implementation: ImageMinimizerPlugin.imageminGenerate,
+          options: {
+            plugins: [["webp", { quality: 75 }]],
+          },
+        },
+      ],
+    }),
+  ],
 });
