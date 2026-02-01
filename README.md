6d1cb3a17d2fd7519a6809ab03e2dc196e88abce
Peacefulkind <68757766+WagokiBry@users.noreply.github.com>
2026-02-01 09:06:59 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This template will be applicable for webpack and linters. It is also applicable for testing with jest
---
+++ eslint.config.mjs
@@ -0,0 +1,60 @@
+import js from "@eslint/js";
+import globals from "globals";
+import json from "@eslint/json";
+import markdown from "@eslint/markdown";
+import css from "@eslint/css";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  // JS files
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "warn",
+    },
+  },
+  // Jest test files
+  {
+    files: ["**/*.test.js", "**/*.spec.js", "**/__tests__/**/*.js"],
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+      },
+    },
+  },
+  // JSON, Markdown, CSS
+  {
+    files: ["**/*.json"],
+    plugins: { json },
+    language: "json/json",
+    extends: ["json/recommended"],
+  },
+  {
+    files: ["**/*.md"],
+    plugins: { markdown },
+    language: "markdown/commonmark",
+    extends: ["markdown/recommended"],
+  },
+  {
+    files: ["**/*.css"],
+    plugins: { css },
+    language: "css/css",
+    extends: ["css/recommended"],
+  },
+]);
+
+// module.exports = {
+//   env: {
+//     jest: true,
+//   },
+// };
+
